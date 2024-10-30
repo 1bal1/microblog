a46d69dd3ec12c2b3fad0830ee149caae3077ac6
bal <1bal1@mail.ru>
2024-10-30 16:07:48 +0300

    chapter 5

--- app/forms.py
+++ app/forms.py
@@ -6,24 +6,24 @@ from app import db
 from app.models import User
 
 class LoginForm(FlaskForm):
-  username = StringField('Пользователь', validators=[DataRequired()])
-  password = PasswordField('Пароль', validators=[DataRequired()])
-  remember_me = BooleanField('Запомнить')
-  submit = SubmitField('Войти')
+    username = StringField('Пользователь', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить')
+    submit = SubmitField('Войти')
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+    username = StringField('Пользователь', validators=[DataRequired(message='Поле обязательно для заполнения')])
+    email = StringField('Почта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(message='Поле обязательно для заполнения')])
+    password2 = PasswordField('Повтор пароля', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
 
     def validate_username(self, username):
         user = db.session.scalar(sa.select(User).where(User.username == username.data))
         if user is not None:
-            raise ValidationError('Please use a different username.')
+            raise ValidationError('Имя пользователя уже используется')
 
     def validate_email(self, email):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError('Почтовый адрес уже используется')
